2a1282a6a16b061703925eb68e71b4317fa3f613
baxiang <baxiang@roobo.com>
2017-09-27 17:41:08 +0800

    增加全局的.h 文件

---
+++ RBUIKit/RBUIKit.h
@@ -0,0 +1,35 @@
+//
+//  RBUIKit.h
+//  Pods
+//
+//  Created by baxiang on 2017/9/27.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#if __has_include(<RBUIKit/RBUIKit>)
+#import <RBUIKit/RBBaseViewController.h>
+#import <RBUIKit/RBNavigationController.h>
+#import <RBUIKit/RBTabBarViewController.h>
+#import <RBUIKit/RBTabBarViewController.h>
+#import <RBUIKit/UIButton+RBExtension.h>
+#import <RBUIKit/UIColor+RBExtension.h>
+#import <RBUIKit/UIImage+RBExtension.h>
+#import <RBUIKit/UIScrollView+RBExtension.h>
+#import <RBUIKit/UITextField+RBExtension.h>
+#import <RBUIKit/UIView+RBDraggable.h>
+#import <RBUIKit/UIView+RBFrame.h>
+#else
+#import "RBBaseViewController.h"
+#import "RBNavigationController.h"
+#import "RBTabBarViewController.h"
+#import "RBTabBarViewController.h"
+#import "UIButton+RBExtension.h"
+#import "UIColor+RBExtension.h"
+#import "UIImage+RBExtension.h"
+#import "UIScrollView+RBExtension.h"
+#import "UITextField+RBExtension.h"
+#import "UIView+RBDraggable.h"
+#import "UIView+RBFrame.h"
+#endif
+
